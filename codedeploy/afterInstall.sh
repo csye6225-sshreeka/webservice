315c051f1413902849b62f24adb7adf589c60651
Shreekara <ss.s@northeastern.edu>
2022-04-04 21:10:04 -0400

    Update codedeploy/afterInstall.sh Signed-off-by: Shreekara <ss.s@northeastern.edu>

--- codedeploy/afterInstall.sh
+++ codedeploy/afterInstall.sh
@@ -5,3 +5,8 @@ sudo systemctl stop app.service
 #removing previous build ROOT folder
 sudo rm -rf /home/ec2-user/*.jar
 
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/ec2-user/cloudwatch_config.json -s
+sudo /bin/systemctl status amazon-cloudwatch-agent
+sudo /bin/systemctl stop amazon-cloudwatch-agent
+sudo /bin/systemctl start amazon-cloudwatch-agent
+

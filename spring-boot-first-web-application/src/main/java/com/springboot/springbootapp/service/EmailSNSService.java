040b73b6ccc241842bf2cb1d0c67c6b3003e8039
Shreekara <ss.s@northeastern.edu>
2022-04-13 20:30:41 -0400

    U Signed-off-by: Shreekara <ss.s@northeastern.edu>

--- spring-boot-first-web-application/src/main/java/com/springboot/springbootapp/service/EmailSNSService.java
+++ spring-boot-first-web-application/src/main/java/com/springboot/springbootapp/service/EmailSNSService.java
@@ -43,125 +43,51 @@ import java.util.Map;
 
 import java.util.Random;
 
+
 @Service
 public class EmailSNSService {
-    //private AmazonSNS snsClient;
-	
-	private SnsClient snsClient;
-	
+    SnsClient snsClient;
+    @Value("${aws.sns.topic.MailNotification.ARN}")
+    String snsTopicARN;
     private final static Logger logger = LoggerFactory.getLogger(EmailSNSService.class);
-    String topicArn;
-       
-    @Autowired
-    public EmailSNSService(){
-    //	 InstanceProfileCredentialsProvider mInstanceProfileCredentialsProvider = new InstanceProfileCredentialsProvider();
-    	//		 AWSCredentials credentials = mInstanceProfileCredentialsProvider.getCredentials();
-    			  //  InstanceProfileCredentialsProvider.createAsyncRefreshingProvider(true);
-    	 
-    	 //AKIA3RMFXDPBGRH6KRNY
-    	 //SyFOyiOrt5qbshSQbu01aX5TbMaWuO58BgkatYcn
-       // this.snsClient =  AmazonSNSClientBuilder.defaultClient();
-        		//.standard()
-        		//.withRegion("us-east-1")
-        		//.withCredentials((AWSCredentialsProvider) credentials)
-        		//.build();//defaultClient();
-
-    	SnsClient snsClient = SnsClient.builder()
-                .region(Region.US_EAST_1)                
-                .build();
-    	//"us-east-1"
-    	CreateTopicRequest request = CreateTopicRequest.builder()
-                .name("Notification_Email")
-                .build();
-
-    	CreateTopicResponse result = null;
-        result = snsClient.createTopic(request);
-//        CreateTopicResult topic = snsClient.createTopic(
-//                new CreateTopicRequest()
-//                        .withName("Notification_Email"));
-//        AmazonS3 amazonS3Client = AmazonS3ClientBuilder.standard()
-//                .withRegion(region)
-//                .withCredentials(new InstanceProfileCredentialsProvider(false))
-//                .build();
-        topicArn = result.topicArn();
-        System.out.println("topicrn: "+topicArn);
-    }
-
-    public void postToTopic(String requestType, String recipientEmail){
+    public void postToTopic(String recipientEmail, String requestType) {
         try {
-        	System.out.println("in sns postToTopic");
-        	Random rand = new Random(); //instance of random class
-            int upperbound = 2500;
-              //generate random values from 0-2499
-            int int_random = rand.nextInt(upperbound);
-
-            String message1 = requestType + "|" + recipientEmail + "|" +
-                    "http://demo.sshreeka.me/v1/verifyUserEmail?email=" + recipientEmail + "&token="+ int_random
-                    + "|" + "messagetype-String" + "|" + int_random;
-            logger.info("stinrg is");
-            logger.info(message1);
-            System.out.println("message generated, now publishing");	
-            PublishRequest publishReq = PublishRequest.builder()
-            		.message(message1)
-            		.topicArn(topicArn)
-            		.build();
-            if(publishReq == null)
-            {
-            	System.out.println("PublishRequest object is null");
+            System.out.println("in sns postToTopic");
+            Random rand = new Random();
+            int randomInt = rand.nextInt(10000);
+            String snsMessage = requestType + "|" + recipientEmail + "|" +
+                    "https://demo.sshreeka.me/v1/verifyUserEmail?email=" + recipientEmail + "&token=" + randomInt
+                    + "|" + "messagetype-String" + "|" + randomInt;
+            System.out.println("message generated, now publishing");
+            PublishRequest request = PublishRequest.builder()
+                    .message(snsMessage)
+                    .topicArn(snsTopicARN)
+                    .build();
+            if (snsClient == null) {
+                System.out.println("snsClient object is still   ..........null");
             }
-            System.out.println("PublishRequest done");
-//            		(topicArn,
-//                    requestType + "|" + recipientEmail + "|" +
-//                    "http://prod.aditijain2025.me/v1/verifyUserEmail?email=" + recipientEmail + "&token="+ int_random
-//                    + "|" + "messagetype-transactional");
-            
-            
-            if(snsClient == null)
-            {
-            	System.out.println("snsClient object is null");
-            	snsClient = SnsClient.builder()
-                        .region(Region.US_EAST_1)                
-                        .build();
-            	CreateTopicRequest request = CreateTopicRequest.builder()
-                        .name("Notification_Email")
-                        .build();
-
-            	CreateTopicResponse result = null;
-                result = snsClient.createTopic(request);
-                topicArn = result.topicArn();
-                System.out.println("topicrn: "+topicArn);
-            }
-            
-            if(snsClient == null)
-            {
-            	System.out.println("snsClient object is still   ..........null");
-            }
-            PublishResponse result = snsClient.publish(publishReq);
+            SnsClient snsClient = SnsClient.builder()
+                    .region(Region.US_EAST_1)
+                    .build();
+            PublishResponse result = snsClient.publish(request);
             System.out.println("Publishing done");
-            System.out.println("Message "+ result.messageId() + "is successfully published to SNS Topic 'Notification_Email'");	
-            logger.info("Message "+ result.messageId() + " is successfully published to SNS Topic 'Notification_Email'.");
+            System.out.println("Message " + result.messageId() + "is successfully published to SNS Topic 'Notification_Email'");
+            //logger.info("Message " + result.messageId() + " is successfully published to SNS Topic 'Notification_Email'.");
             AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().build();
             DynamoDB dynamoDb = new DynamoDB(client);
-            Table table = dynamoDb.getTable("ass");
-//            PutItemRequest request = new PutItemRequest().withTableName("TokenTable").withReturnConsumedCapacity("TOTAL");
-//            PutItemResult _response = client.putItem(request);
-//            Map<String,Object> user_token = new HashMap<String, Object>();
-//            user_token.put("emailID", recipientEmail);
-//            user_token.put("expiration_time", (int_random));
+            Table table = dynamoDb.getTable("UsernameTokenTable");
+//
             long now = Instant.now().getEpochSecond(); // unix time
             long ttl = 300; // 24 hours in sec
             Item item = new Item()
                     .withPrimaryKey("emailID", recipientEmail)
-                    .with("Token",int_random)
+                    .with("Token",randomInt)
                     .with("TimeToLive",ttl + now);
             PutItemOutcome outcome = table.putItem(item);
-
         } catch (SnsException e) {
-        	System.out.println("sns exception: "+e.getMessage());
-        	 e.printStackTrace();
-        	
+            System.out.println("sns exception: " + e.getMessage());
+            e.printStackTrace();
             logger.error("SNS Exception Warning - " + e.getMessage());
         }
     }
 }
-

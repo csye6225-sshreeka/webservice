34bec931ce89a1d126742bfbc99e47448e184fe1
Shreekara <ss.s@northeastern.edu>
2022-04-14 20:39:55 -0400

    U
    Signed-off-by: Shreekara <ss.s@northeastern.edu>

--- spring-boot-first-web-application/src/main/java/com/springboot/springbootapp/service/EmailSNSService.java
+++ spring-boot-first-web-application/src/main/java/com/springboot/springbootapp/service/EmailSNSService.java
@@ -69,19 +69,7 @@ public class EmailSNSService {
             System.out.println("Message " + result.messageId() + "is successfully published to SNS Topic 'Notification_Email'");
             logger.info("Message " + result.messageId() + " is successfully published to SNS Topic 'Notification_Email'.");
             logger.info(snsMessage);
-//            AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().build();
-//            DynamoDB dynamoDb = new DynamoDB(client);
-//            Table table = dynamoDb.getTable("UsernameTokenTable");
-////
-//            long now = Instant.now().getEpochSecond(); // unix time
-//            long ttl = 300; // 24 hours in sec
-//
-//            Item item = new Item()
-//                    .withPrimaryKey("emailID", recipientEmail)
-//                    .with("TimeToLive",now+ttl)
-//                    .with("Token",randomInt);
-//           // client.updateTimeToLive(add5Min());
-//            PutItemOutcome outcome = table.putItem(item);
+
         } catch (SnsException e) {
             System.out.println("sns exception: " + e.getMessage());
             e.printStackTrace();
